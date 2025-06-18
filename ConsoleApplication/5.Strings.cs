03a3b7c251f5b3e3ff848abab93f81c1209f2004
ushaarpitha-02 <ushaarpitha.fullstack@gmail.com>
2025-06-18 18:44:44 +0530

    strings1

--- ConsoleApplication/5.Strings.cs
+++ ConsoleApplication/5.Strings.cs
@@ -3,6 +3,7 @@ class Strings
 {
     static void Main()
     {
+        Strings1();
         string name = "Usha";
         Console.WriteLine($"My name is : {name} Arpitha");
 
@@ -29,6 +30,73 @@ class Strings
         var myFriendStudies = "CA";
         var myFriendFamily= "Father, Mother";
         Console.WriteLine($"My friend's name is {myFriend}, she is {myFriendAge} years old, she is studying {myFriendStudies} and her family members are {myFriendFamily}.");
+        
+
+
+
+    }
+    static void Strings1()
+    {
+        string sentence = "Human Being should show some Humanity if they want to be called as Human Being";
+        Console.WriteLine(sentence);// We can use this in other format also as shown in next statement
+        Console.WriteLine($"{sentence}");
+        int length = sentence.Length;
+        Console.WriteLine("Length of the sentence is:" + length);
+
+        string lowercase = sentence.ToLower();
+        Console.WriteLine($"Lowercase of the sentence is:{lowercase}");
+
+
+        string uppercase = sentence.ToUpper();
+        Console.WriteLine($"Uppercase of the sentence is:{uppercase}");
+
+        string name = "     CSHARP     ";
+        Console.WriteLine(name);
+
+        Console.WriteLine($"Trimming the string is:{name.Trim()}");
+        Console.WriteLine($"Trimming the begin of the string is:{name.TrimStart()}");
+        Console.WriteLine($"Trimming the end of the string is:{name.TrimEnd()}");
+
+        Console.WriteLine($"Padding the string is:{name.PadLeft(20, '*')}");
+        Console.WriteLine($"Padding the string is:{name.PadRight(20, '*')}");
+
+
+        string password = "Usha@123";
+        Console.WriteLine(password);
+        bool isequal = password.Equals("Usha#123");
+        Console.WriteLine($"Is the password equal to Usha@123? {isequal}");
+
+        string sentence1= "software engineers are going to shine in the future";
+        Console.WriteLine(sentence1);
+        Console.WriteLine($"Length of the sentence1 is:{sentence1.Length}");
+       
+        string substring = sentence1.Substring(0, 8);
+        Console.WriteLine($"Substring of the sentence is: {substring}");
+        Console.WriteLine($"Substring of the sentence is: {sentence1.Substring(9, 8)}");
+        
+        int index = sentence1.IndexOf("shine");
+        Console.WriteLine($"Index of the word 'shine' in the sentence is: {index}");
+        string[] words = sentence1.Split(' ');
+        Console.WriteLine("Words in the sentence are:");
+        foreach (string word in words)
+        {
+            Console.WriteLine(word);
+        }
+
+        string profession= "Software Engineer";
+        Console.WriteLine($"Profession is: {profession}");
+        string replacedstring = profession.Replace("Engineer", "Developer");
+        Console.WriteLine($"Replaced string is :{replacedstring}");
+
+        int quantity = 19;
+        double price = 79.8;
+
+        string receipt = string.Format("You purcahsed {0} items for {1}", quantity, quantity * price);
+        Console.WriteLine(receipt);
+
+        string data = "apple,banana,cherry";
+        string[] fruitsList = data.Split(',');
+        Console.WriteLine(fruitsList);
 
 
 
---
+++ Practice programs/Practice programs/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+
+namespace Practice_programs
+{
+     class Program
+    {
+        static void Main()
+        {
+            int a = 5;
+            int b = a++;
+            Console.WriteLine($"{a}, {b}");
+
+
+
+            
+
+
+            int p = 5;
+            double q = a / 2;
+            Console.WriteLine(b); //What is printed?
+
+
+
+            string c = "";
+            string d = null;
+            Console.WriteLine(c == d); //What’s printed?
+
+            bool isTrue = false;
+            if (isTrue = true)
+            {
+                Console.WriteLine("True!");// Will this print "True!" ?
+            }
+
+
+
+
+            int x = 10;
+            {
+                int x = 20; //Will this compile?
+                Console.WriteLine(x);
+            }
+        }
+    }
+}
