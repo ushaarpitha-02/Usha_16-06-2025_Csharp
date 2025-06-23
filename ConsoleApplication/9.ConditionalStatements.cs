9080e0ff2e4e67e941203c03738cb899e1feb41b
ushaarpitha-02 <ushaarpitha.fullstack@gmail.com>
2025-06-23 17:26:51 +0530

    Statements

--- ConsoleApplication/9.ConditionalStatements.cs
+++ ConsoleApplication/9.ConditionalStatements.cs
@@ -169,7 +169,7 @@ class ConditionalStatements
     {
         Console.WriteLine("Enter your age");
         int age = Convert.ToInt32(Console.ReadLine());
-        if (age>=18)
+        if (age >= 18)
         {
             Console.WriteLine("You are eligible to vote");
         }
