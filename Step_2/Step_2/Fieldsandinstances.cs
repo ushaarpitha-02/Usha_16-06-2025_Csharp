81a9b9e5c6cad170f2b2b111e4f7f54a0443071e
ushaarpitha-02 <ushaarpitha.fullstack@gmail.com>
2025-07-03 12:08:11 +0530

    step-2

---
+++ Step_2/Step_2/Fieldsandinstances.cs
@@ -0,0 +1,47 @@
+﻿using System;
+class EmployeeDetails
+{
+    int empid = 101;
+    string empname = "Usha";
+    int empage = 25;
+    string empjoindate = "May-23-2025";
+
+    
+    static void Main()
+    {
+        EmployeeDetails student = new EmployeeDetails()
+        {
+            empid = 103,
+            empname = "Arpitha"
+        };
+        showdetails(student);
+        EmployeeDetails employeeDetails = new EmployeeDetails();
+        Console.WriteLine(employeeDetails.empid);
+
+        employeeDetails.empid = 102;
+        employeeDetails.empname = "Vnitha";
+        employeeDetails.empage = 26;
+        employeeDetails.empjoindate = "Jun-1-2025";
+        Console.WriteLine(employeeDetails.empage);
+
+        employeeDetails.ShowMessage("ush", 24);
+        string name = ShowDetails("ushaaa", 103);
+
+
+
+    }
+
+    static string ShowDetails(string name, int id)
+    {
+        Console.WriteLine($"Name is {name} id is {id}");
+        return name;
+    }
+    void ShowMessage(string empname, int empid)
+    {
+        Console.WriteLine($"name is {empname} id is {empid}");
+    }
+    static void showdetails(EmployeeDetails emp)
+    {
+        Console.WriteLine($"emp name is {emp.empname} emp id is {emp.empid}");
+    }
+}
---
+++ Step_2/Step_2/Properties.cs
@@ -0,0 +1,36 @@
+﻿using System;
+class EventManager
+{
+
+    string eventname = "haldi";
+    string eventlocation = "bangalore";
+    string eventorganizer = "Usha";
+    string eventdate = "1st june";
+
+
+    string Eventname { get; set; }
+    string Eventlocation { get; set; }
+    string Eventorganizer { get; set; }
+    string Eventdate { get; set; }
+
+    static void Main()
+    {
+        EventManager events=new EventManager();
+        Console.WriteLine($"Event name is {events.eventname}\nEvent location is {events.eventlocation}\nEvent organizer is {events.eventorganizer}\nEvent date is on {events.eventdate}");
+
+        events.Eventname = "mehendi";
+        events.Eventlocation = "hyderabad";
+        events.Eventorganizer = "Arpitha";
+        events.Eventdate = "2nd june";
+
+        Console.WriteLine($"Event name is {events.Eventname}\nEvent location is {events.Eventlocation}\nEvent organizer is {events.Eventorganizer}\nEvent date is on {events.Eventdate}");
+    }
+}
+
+
+
+
+
+
+
+
